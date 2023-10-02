8d27e386e31d6a78d1b5b77f0922d4baf503c677
Anna Padun <roananik1988@gmail.com>
2023-10-02 16:01:39 +0200

    HW_09 ArrayValueCalculator fix

--- src/main/java/com/roananik/lessons12/ArrayValueCalculator.java
+++ src/main/java/com/roananik/lessons12/ArrayValueCalculator.java
@@ -1,90 +1,45 @@
 package com.roananik.lessons12;
 
-import java.util.Arrays;
 
 public class ArrayValueCalculator {
-   // private static int arrayLineIndex = 0;
 
-
-    public static void main(String[] args) throws ArraySizeException {
-        String[][] s = new String[][]{
-                {"1", "1", "1","1"},
-                {"1", "1", "1", "1"},
-                {"1", "1", "1", "1"},
-                {"1", "1", ".", "1"}
-        };
-
-
-        try {
-            System.out.println("sum:"+doCalc(s));
-        } catch (ArrayDataException | ArraySizeException e){
-            System.out.println(e.getMessage());
-        }
-
-
-    }
 
     public static int doCalc(String[][] data) throws ArraySizeException {
         sizeCheck(data);
         int sum = 0;
-        for(int i=0;i<data.length;i++){
-            for(int j=0;j<data[i].length;j++){
-                sum+=convertSymbol(data[i][j],i,j);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+
+                sum += convertSymbol(data[i][j], i, j);
 
             }
         }
-return sum;
-
-    }
-
-    private static int convertSymbol(String symbol,int height, int index){
-        int sum=0;
-        try {
-            sum+=Integer.parseInt(symbol);
-        }catch (NumberFormatException e){
-            // System.out.println("wrong sumbol --"+symbol+ "-- position:"+"["+line+"]"+"["+hige+"]");
-           throw new ArrayDataException("wrong sumbol --"+symbol+ "-- position:"+"["+height+"]"+"["+index+"]");
-        }
         return sum;
+
     }
 
-//    public static int doCalc(String[][] data) throws ArraySizeException {
-//        sizeCheck(data);
-//
-//        return Arrays.stream(data).mapToInt(ArrayValueCalculator::getConvertDataLineSum).sum();
-//
-//    }
+
+    private static int convertSymbol(String symbol, int height, int index) throws ArrayDataException {
+        int convertResult;
+        try {
+
+            convertResult = Integer.parseInt(symbol);
+        } catch (NumberFormatException e) {
+
+            throw new ArrayDataException("wrong symbol --" + symbol +
+                    "-- position:" + "[" + height + "]" + "[" + index + "]");
+        }
+        return convertResult;
+    }
+
 
     private static void sizeCheck(String[][] data) throws ArraySizeException {
-        if (data.length != 4) throw new ArraySizeException("wrong size!!");
+        if (data.length != 4) throw new ArraySizeException("wrong array size!!");
         for (String[] datum : data) {
-            if (datum.length != 4) throw new ArraySizeException("wrong size!!");
+            if (datum.length != 4) throw new ArraySizeException("wrong array size!!");
         }
 
     }
-
-//    private static int getConvertDataLineSum(String[] line) throws ArrayDataException {
-//         int lineSum = 0;
-//
-//        for (int i = 0; i < line.length; i++) {
-//
-//            try {
-//                lineSum += Integer.parseInt(line[i]);
-//            } catch (ArrayDataException e) {
-//             // System.out.println("wrong index " + "[" + arrayLineIndex + "]" + "[" + i + "]");
-//               throw new ArrayDataException("wrong index ");
-//
-//            }catch (NumberFormatException e){
-//                System.out.println("wrong index " + "[" + arrayLineIndex + "]" + "[" + i + "]");
-//
-//            }
-//        }
-//        arrayLineIndex++;
-//
-//
-//        return lineSum;
-//
-//    }
 
 
 }
