4459a3a3ceed9dad40b7db2d99622080f7b1bb8c
Anna Padun <roananik1988@gmail.com>
2023-10-05 16:33:01 +0200

    HW_10 BookAnalize fix

--- src/main/java/com/roananik/lessons13/BookAnalize.java
+++ src/main/java/com/roananik/lessons13/BookAnalize.java
@@ -6,93 +6,99 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class BookAnalize implements Runnable {
-    private StringBuilder allInfo = new StringBuilder();
-    private final String nameBook;
-    private String[] bookData;
 
-    private final int countLimit;
-    Map<String, Integer> map = new HashMap<>();
+public class BookAnalize implements Runnable {
+    private final String infoMessage = " Unique words:\n";
+
+    private final StringBuilder bookStatistics = new StringBuilder();
+    private final String nameBook;
+    private String[] allWordsFromBook;
+    private final int numberOfWordsNeeded;
+    Map<String, Integer> wordsAndNumberOfRepetitions = new HashMap<>();
 
 
     public BookAnalize(String nameBook, int countLimit) {
         this.nameBook = nameBook;
-        this.countLimit = countLimit;
+        this.numberOfWordsNeeded = countLimit;
+    }
+
+    @Override
+    public void run() {
+        String pathFile = "src/" + this.nameBook + ".txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathFile))) {
+            String line;
+            StringBuilder temporaryResult = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                temporaryResult.append(line).append(" ");
+
+            }
+            line = temporaryResult.toString().toLowerCase();
+            line = line.replaceAll("[^a-zA-Zа-яА-Я]", " ");
+            allWordsFromBook = line.split("\\s+");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("book not found");
+            return;
+        } catch (IOException e) {
+            System.out.println("Something went wrong");
+            return;
+        }
+
+        getSpecifiedNumberOfWords();
+        amountOfAllUniqueWords();
+        bookStatistics.append("name file: " + this.nameBook + "_statistic.txt");
+        createStatisticFile();
+    }
+
+
+    private void getSpecifiedNumberOfWords() {
+
+        for (String word : allWordsFromBook) {
+            if (word.length() > 2)
+                wordsAndNumberOfRepetitions
+                        .put(word, wordsAndNumberOfRepetitions.getOrDefault(word, 0) + 1);
+        }
+
+
+        List<Map.Entry<String, Integer>> popularWords = new ArrayList<>(wordsAndNumberOfRepetitions.entrySet());
+        popularWords.sort((a, b) -> b.getValue()
+                .compareTo(a.getValue()));
+
+        List<String> selectedWords = popularWords.stream()
+                .map(p -> p.getKey())
+                .limit(this.numberOfWordsNeeded)
+                .toList();
+
+        System.out.println(numberOfWordsNeeded + infoMessage + selectedWords);
+
+        bookStatistics.append(numberOfWordsNeeded).append(infoMessage);
+
+        for (var word : selectedWords) {
+            bookStatistics.append(word).append(": ")
+                    .append(wordsAndNumberOfRepetitions.get(word)).append(". \n");
+        }
+
+
+    }
+
+    private void amountOfAllUniqueWords() {
+        int count = wordsAndNumberOfRepetitions.size();
+
+        System.out.println(infoMessage + "total: " + count);
+        bookStatistics.append(infoMessage + "total: ").append(count).append("\n");
     }
 
 
     private void createStatisticFile() {
-        String names = this.nameBook + "_statistic.txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(names))) {
-            writer.write(allInfo.toString());
+        String statisticFile = this.nameBook + "_statistic.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(statisticFile))) {
+            writer.write(bookStatistics.toString());
 
 
         } catch (IOException e) {
-            System.out.println("som wrong");
+            System.out.println("Something went wrong");
         }
-    }
-
-
-    private void unik() {
-        int count = map.size();
-//        for (var w : map.entrySet()) {
-//            if (w.getKey().length() > 1) {
-//                count++;
-//            }
-//
-//        }
-        System.out.println("Unique words in total: " + count);
-        allInfo.append("Unique words in total: ").append(count).append("\n");
-    }
-
-    private void countWords() {
-
-        for (String word : bookData) {
-
-            if (word.length() > 3)
-                map.put(word, map.getOrDefault(word, 0) + 1);
-        }
-        List<Map.Entry<String, Integer>> popularWords = new ArrayList<>(map.entrySet());
-        popularWords.sort((a, b) -> b.getValue().compareTo(a.getValue()));
-
-        List<String> result10Words = popularWords.stream().map(p -> p.getKey()).limit(this.countLimit).toList();
-        System.out.println(countLimit + " unique words:\n" + result10Words);
-        allInfo.append(countLimit + " Unique words:\n");
-        for (var res : result10Words) {
-            allInfo.append(res).append(": ").append(map.get(res)).append(". \n");
-        }
-
-
-    }
-
-
-    @Override
-    public void run() {
-        String adres = "src/" + this.nameBook + ".txt";
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(adres))) {
-            String line;
-            StringBuilder bilder = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                bilder.append(line).append(" ");
-
-            }
-            line = bilder.toString().toLowerCase();
-            line = line.replaceAll("[^a-zA-Zа-яА-Я]", " ");
-            bookData = line.split("\\s+");
-
-        } catch (FileNotFoundException e) {
-            System.out.println("file not faund");
-            return;
-        } catch (IOException e) {
-            System.out.println("no conect");
-            return;
-        }
-
-        countWords();
-        unik();
-        allInfo.append("name file: " + this.nameBook + "_statistic.txt");
-        createStatisticFile();
     }
 
 
--- src/main/java/com/roananik/lessons13/Main.java
+++ src/main/java/com/roananik/lessons13/Main.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-Scanner scanner =new Scanner(System.in);
-        System.out.println("enter the book title");
-        String bookTitle=scanner.nextLine();
-        System.out.println("enter the number. hom many unique words do you want to get");
-        int count= scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-        BookAnalize bookAnalize = new BookAnalize(bookTitle,count);
+        System.out.println("enter the book title");
+        String bookTitle = scanner.nextLine();
+
+        System.out.println("enter the number. hom many unique words do you want to get");
+        int numberOfUniqueWords = scanner.nextInt();
+
+        scanner.close();
+
+        BookAnalize bookAnalize = new BookAnalize(bookTitle, numberOfUniqueWords);
         bookAnalize.run();
 
     }
