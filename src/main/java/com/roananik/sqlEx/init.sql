dd2c1104d618663d11f2c650502674520134f647
Anna Padun <roananik1988@gmail.com>
2023-11-20 17:50:09 +0100

    sql fix

--- src/main/java/com/roananik/sqlEx/init.sql
+++ src/main/java/com/roananik/sqlEx/init.sql
@@ -1,19 +1,4 @@
-/*Створити таблицю Homework. Ця таблиця складається
-  з атрибутів: id, name, description
-Для атрибуту ID має бути забезпечене обмеження Primary Key та AutoIncrement
 
-Створити таблицю Lesson. Ця таблиця складається з атрибутів: id, name, updatedAt,
-  homework_id (зв'язок one-to-one)
-Для атрибуту ID має бути забезпечене обмеження Primary Key та AutoIncrement
-Пов'язано з таблицею Homework через Foreign Key
-
-Створити таблицю Schedule. Ця таблиця складається з атрибутів: id, name,
-  updatedAt, lessons (зв'язок many-to-many)
-Для атрибуту ID має бути забезпечене обмеження Primary Key і AutoIncrement
-Зв'язано з таблицею Lesson через Foreign Key
-
- Перенести ініціалізаційні sql-скрипти у файл initsql
-. ! Файл init.sql і є результатом завдання, яке має бути додане як рішення */
 CREATE  TABLE IF NOT EXISTS homework(
     id INT Primary Key,
     name VARCHAR(50),
@@ -39,6 +24,6 @@ CREATE  TABLE IF NOT EXISTS schedule_lessons(
     lesson_id INT,
     schedule_id INT,
     PRIMARY KEY(schedule_id,lesson_id),
-    FOREIGN KEY(schedule_id) REFERENCES schedule(id) ON DELETE CASCADE,
+    FOREIGN KEY(schedule_id) REFERENCES schedule(id) ON DELETE CASCADE,/*зв'язок many-to-many*/
     FOREIGN KEY(lesson_id) REFERENCES lesson(id) ON DELETE CASCADE
      );
